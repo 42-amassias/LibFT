e3fde701fca65df3ee42cb5d34cf923e8021798d
Antoine Massias <massias.antoine@gmail.com>
2023-10-16 14:28:36 +0200

    Fixed strlcpy.

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,28 +6,28 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 10:16:32 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/16 14:23:20 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:28:18 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
+size_t	ft_strlcpy(void *dest, const void *src, size_t size)
 {
-	unsigned int	len;
-	char			*dst;
+	size_t	len;
+	void	*dst;
 
 	len = 0;
 	dst = dest;
-	while (*src && size > 1)
+	while (*(char *)src && size > 1)
 	{
-		*dst++ = *src++;
+		*(char *)dst++ = *(char *)src++;
 		--size;
 		++len;
 	}
 	while (size--)
-		*dst++ = '\0';
-	while (*src++)
+		*(char *)dst++ = '\0';
+	while (*(char *)src++)
 		++len;
 	return (len);
 }
