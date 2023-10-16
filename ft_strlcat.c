f3291a0d6f65fcc6ce5e21c5028850d2175978d5
Antoine Massias <massias.antoine@gmail.com>
2023-10-16 14:28:49 +0200

    Fixed strlcat.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,37 +6,37 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:06:34 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/16 14:24:28 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:27:51 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+size_t	ft_strlcat(void *dest, const void *src, size_t size)
 {
-	char			*dst_cpy;
-	const char		*src_cpy;
-	unsigned int	remaining;
-	unsigned int	dest_len;
+	void		*dst_cpy;
+	const void	*src_cpy;
+	size_t		remaining;
+	size_t		dest_len;
 
 	dst_cpy = dest;
 	src_cpy = src;
 	remaining = size;
-	while (remaining-- && *dst_cpy)
+	while (remaining-- && *(char *)dst_cpy)
 		dst_cpy++;
 	dest_len = dst_cpy - dest;
 	remaining = size - dest_len;
 	if (!remaining)
 		return (dest_len + ft_strlen(src_cpy));
-	while (*src_cpy)
+	while (*(char *)src_cpy)
 	{
 		if (remaining != 1)
 		{
-			*dst_cpy++ = *src_cpy;
+			*(char *)dst_cpy++ = *(char *)src_cpy;
 			--remaining;
 		}
 		src_cpy++;
 	}
-	*dst_cpy = '\0';
+	*(char *)dst_cpy = '\0';
 	return (dest_len + (src_cpy - src));
 }
