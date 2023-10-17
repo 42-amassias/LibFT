b92ec13627cfe208fc4f3e85d0826ebe3626c2a9
Antoine Massias <massias.antoine@gmail.com>
2023-10-18 00:15:47 +0200

    Fixed strncmp. On some occasions it returned the wrong value.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:39:30 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/16 21:55:41 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/18 00:13:58 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (n--)
 	{
-		if (*s1 == '\0' || *s1 != *s2)
-			return (*s1 - *s2);
+		if (*s1 == '\0' || *(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		s1++;
 		s2++;
 	}
