0df60de62f0bffb79222278e8296da42f70d3678
Antoine Massias <massias.antoine@gmail.com>
2023-10-18 00:28:03 +0200

    Fixed substr. It was not behaving correctly when start >= strlen(s).

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 19:21:28 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/17 19:24:05 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/18 00:26:43 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,14 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*copy;
+	size_t	tmp;
 
+	tmp = ft_strlen(s);
+	if (start >= tmp)
+		return (ft_strdup(""));
+	tmp -= start;
+	if (tmp < len)
+		len = tmp;
 	copy = (char *) malloc(len + 1);
 	if (copy)
 	{
