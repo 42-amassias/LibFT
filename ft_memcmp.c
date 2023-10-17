fbd0ba67bd7706fd1686f553d2524b28c8af3093
Antoine Massias <massias.antoine@gmail.com>
2023-10-18 00:17:36 +0200

    Fixed memcmp. On some occasions it returned the wrong value.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 21:56:29 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/16 21:57:33 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/18 00:16:30 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	while (n--)
 	{
-		if (*(char *)s1 != *(char *)s2)
-			return (*(char *)s1 - *(char *)s2);
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		s1++;
 		s2++;
 	}
