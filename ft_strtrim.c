492973162edbe1c2d00ba71c5f296e13d8ddf756
Antoine Massias <massias.antoine@gmail.com>
2023-10-18 01:15:09 +0200

    Fixed strtrim. I'm a dumbass.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 22:14:55 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/18 00:29:48 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/18 01:14:07 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strtrim(char const *str, char const *set)
 	char	*trimmed;
 	size_t	len;
 
-	while (*str && ft_strrchr(set, *str))
+	while (*str && ft_strchr(set, *str))
 		++str;
 	len = ft_strlen(str);
-	while (str[len - 1] && ft_strrchr(set, str[len - 1]))
+	while (*str && ft_strchr(set, str[len - 1]))
 		--len;
 	trimmed = malloc(len + 1);
 	if (trimmed)
