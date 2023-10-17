52813a8af55ca17651977803aa16bf8016a2a3f4
Antoine Massias <massias.antoine@gmail.com>
2023-10-17 19:28:36 +0200

    Added ft_strjoin.

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 12:34:25 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/17 19:21:16 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/17 19:24:58 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strdup(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 // Stdlib
 
