6823f8f432d216c211c0a27a61557fba23eec152
Antoine Massias <massias.antoine@gmail.com>
2023-10-18 03:08:20 +0200

    Added ft_lstsize.

---
+++ _bonus/ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 03:05:48 by amassias          #+#    #+#             */
+/*   Updated: 2023/10/18 03:07:25 by amassias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *list)
+{
+	t_list	**list_ptr;
+	int		size;
+
+	list_ptr = &list;
+	size = 0;
+	while (*list_ptr)
+	{
+		++size;
+		list_ptr = &(*list_ptr)->next;
+	}
+	return (size);
+}
