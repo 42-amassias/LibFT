3d7dd319ab30ef7f8a863af693b6b052e6342f3d
Antoine Massias <massias.antoine@gmail.com>
2023-10-18 02:52:19 +0200

    Added lstnew.

---
+++ _bonus/ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 02:39:24 by amassias          #+#    #+#             */
+/*   Updated: 2023/10/18 02:46:02 by amassias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (list)
+	{
+		list->content = content;
+		list->next = NULL;
+	}
+	return (list);
+}
