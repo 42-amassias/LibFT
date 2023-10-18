af46f52029eef461f1e227e27bdbeb899c2bdec3
Antoine Massias <massias.antoine@gmail.com>
2023-10-18 03:20:13 +0200

    Added ft_lstclear.

---
+++ _bonus/ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 03:14:48 by amassias          #+#    #+#             */
+/*   Updated: 2023/10/18 03:19:42 by amassias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **list, void (*del_fun)(void *))
+{
+	t_list	*l;
+	t_list	*node;
+
+	l = *list;
+	while (l)
+	{
+		node = l;
+		l = node->next;
+		ft_lstdelone(node, del_fun);
+	}
+	*list = NULL;
+}
