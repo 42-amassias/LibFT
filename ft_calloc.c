086fb90b7db7f96f4dff1b603ee016ea4a5e2852
Antoine Massias <amassias@c1r1p1.42lehavre.fr>
2023-10-20 17:48:22 +0200

    Fix: Subject is shit, it asks to implement as specified in the man but you're not allowed to use errno. Consistency ?

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,13 +6,11 @@
 /*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 22:01:26 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/18 02:18:40 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/20 17:46:54 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-#include <errno.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
@@ -23,10 +21,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (nmemb == 0)
 		return (malloc(0));
 	if ((size_t)(-1 / nmemb) < size)
-	{
-		errno = ENOMEM;
 		return (NULL);
-	}
 	ptr = malloc(sz);
 	if (ptr)
 		ft_bzero(ptr, sz);
