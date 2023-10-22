70c380366e9ff60646a102b5ad64ede0a9366244
dyamen1411 <massias.antoine@gmail.com>
2023-10-22 16:41:02 +0200

    Fix: removed unused variable.

--- ft_memswap.c
+++ ft_memswap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memswap.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dyamen <dyamen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:55:02 by amassias          #+#    #+#             */
-/*   Updated: 2023/10/20 17:57:52 by amassias         ###   ########.fr       */
+/*   Updated: 2023/10/22 16:40:06 by dyamen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void	ft_memswap(void *_a, void *_b, size_t n)
 {
 	unsigned char	*a;
 	unsigned char	*b;
-	unsigned char	c;
 
 	a = _a;
 	b = _b;
